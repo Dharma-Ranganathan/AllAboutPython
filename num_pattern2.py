43e97727899273cd677aa20cd5c8321c85b232e5
Dharma-Ranganathan <dharmaqubla369@gmail.com>
2025-07-28 11:53:14 +0530

    pattern 2 commit

---
+++ num_pattern2.py
@@ -0,0 +1,16 @@
+"""
+number patter - 2
+
+11111
+11111
+11111
+11111
+11111
+
+"""
+# here are code to achieve this using for loops
+
+for i in range(0,5):
+    for j in range(0,5):
+        print(1,end='')
+    print()
