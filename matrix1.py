97b2f8263ea0e2f1c22c81f4d76c1dbe5c9e462a
Dharma Ranganathan <dharmaqubla369@gmail.com>
2025-07-28 13:15:44 +0530

    matrix1 commit

---
+++ matrix1.py
@@ -0,0 +1,11 @@
+'''
+first step to start with matrix / 2d arrays.
+'''
+
+# here are codes to print each value of given 2D array
+
+arr2d = [[0,1,2],[3,4,5],[6,7,8,9]]
+# traversing 2d arrays
+for i in range(len(arr2d)):
+    for j in range(len(arr2d[i])):
+        print(arr2d[i][j])
