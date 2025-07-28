ab2c6e0106ac6221e39adf1316a9e62b14cc3352
Dharma-Ranganathan <dharmaqubla369@gmail.com>
2025-07-28 11:37:00 +0530

    pattern1 commit

---
+++ pattern1.py
@@ -0,0 +1,19 @@
+'''
+number pattern - 1
+
+1
+22
+333
+4444
+55555
+
+'''
+
+# here are the code to achieve this using for loops
+
+for i in range(1,6):
+    for j in range(0,i):
+        print(i,end='')
+    print()
+
+
