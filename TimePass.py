26ec21534d4b8cd5d4626bc57db6f6d747734de1
Dharma Ranganathan <dharmaqubla369@gmail.com>
2025-08-02 11:41:55 +0530

    Update TimePass.py

--- TimePass.py
+++ TimePass.py
@@ -3,18 +3,38 @@ form filling using python loops
 
 time passing code..
 """
+import re
+
+email= r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
 
 forms = [
-    "name : ",
-    "age : ",
-    "email : ",
-    "phno : ",
-    "city : ",
+    "name",
+    "age",
+    "email",
+    "phno",
+    "city"
     ]
-
+    
 datas = {}
+
 for i in range(len(forms)):
-    value = input(forms[i])
+    value = input(f"{forms[i]} : ").lower()
     datas[forms[i]] = value
     
-print(datas)
+if datas["age"].isalpha():
+    print("Age cannot be letters..")
+    
+elif not re.fullmatch(email,datas["email"]):
+    print("Invalid Email Input..")
+
+elif len(datas["phno"]) != 10 or datas["phno"].isalpha():
+    print("Phone number cannot be alphabets / less or more than ten..")
+else:
+    yn = input("do you want to submit application ? y/n : ").lower()
+    if yn != "y" and yn != "n":
+        print("Invalid selection..")
+    
+    if yn == "n":
+        print("Form not submitted..")
+    else:
+        print("Form submitted..")
