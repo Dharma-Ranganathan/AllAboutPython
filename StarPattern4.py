6e77a6697c1ee40ff0b4c3fdcf084ec4903c6881
Dharma Ranganathan <dharmaqubla369@gmail.com>
2025-08-02 14:19:56 +0530

    Create StarPattern4.py

---
+++ StarPattern4.py
@@ -0,0 +1,19 @@
+"""
+output :
+
+* * * * *
+  * * * * 
+    * * * 
+      * * 
+        *
+"""
+n = 5
+rows = 0
+for i in range(0,n):
+    for j in range(1,n+1):
+        if j <= rows:
+            print(" ",end=" ")
+            continue
+        print("*",end=" ")
+    print()
+    rows += 1
