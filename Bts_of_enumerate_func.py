f9ff3b06e9d47f8499ca906b042218521e8d21fb
Dharma Ranganathan <dharmaqubla369@gmail.com>
2025-10-27 19:26:02 +0530

    Refactor enumerate function and update print format

--- Bts_of_enumerate_func.py
+++ Bts_of_enumerate_func.py
@@ -1,26 +1,33 @@
-# import numpy
+# behind the scenes of enumerate function 
+
+# numpy
 import numpy as np 
 
 # data
 data = np.random.randint(1,10,5)
+print(data)
 
 # function creating 
 def enumerate_(list_):
-  i = 0
-  while i < len(list_):
-    yield i,list_[i]
-    i += 1
-
+    i = 0
+    while i < len(list_):
+        yield i,list_[i]
+        i += 1
+        
 # function call
-for i,v in enumerate_(data):
-  print(f'index : {i} • value : {v}') 
-
+for i , v in enumerate_(data):
+    print(f'index : {i} • value : {v}')
+    
 """
 output came :
-[6,7,8,4,4]
-index : 0 • value : 6
-...
-...
-...
-index : 4 • value : 4
+
+[8 8 8 4 2]
+index : 0 • value : 8
+index : 1 • value : 8
+index : 2 • value : 8
+index : 3 • value : 4
+index : 4 • value : 2
 """
+
+
+
